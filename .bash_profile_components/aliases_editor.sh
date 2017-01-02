f190b4d72fad444c19737d47c6e2470c512f4332
Jeremy Zornow <jeremy@zornow.com>
2017-01-01 18:08:58 -0700

    Rely on usr/local/bin-linked version of sublime editor
    This is now an environment-specific implementation detail. There
    needs to be a subl symlink in this location. (Which is fine.)

--- .bash_profile_components/aliases_editor.sh
+++ .bash_profile_components/aliases_editor.sh
@@ -1,7 +1,4 @@
 # Wildcard because some machines have Sublime Text and some have Sublime Text 2
-export VISUAL='~/../../Applications/Sublime\ Text*.app/Contents/SharedSupport/bin/subl'
+export VISUAL='usr/local/bin/subl'
 export EDITOR='/usr/bin/vim'
 export GIT_EDITOR='/usr/bin/vim'
-
-
-alias subl=$VISUAL
