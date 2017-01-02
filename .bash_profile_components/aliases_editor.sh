1957c4682756d67f8a4355a62e10a5083d8fb15d
Jeremy Zornow <jeremy@zornow.com>
2017-01-01 17:26:08 -0700

    Ensures GIT_EDITOR is set to VIM.

--- .bash_profile_components/aliases_editor.sh
+++ .bash_profile_components/aliases_editor.sh
@@ -1,3 +1,7 @@
 # Wildcard because some machines have Sublime Text and some have Sublime Text 2
-export EDITOR='~/../../Applications/Sublime\ Text*.app/Contents/SharedSupport/bin/subl'
-alias subl=$EDITOR
+export VISUAL='~/../../Applications/Sublime\ Text*.app/Contents/SharedSupport/bin/subl'
+export EDITOR='/usr/bin/vim'
+export GIT_EDITOR='/usr/bin/vim'
+
+
+alias subl=$VISUAL
